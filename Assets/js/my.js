d78450fb2e492b6cbb1d560ec333c4a79c7a4078
Aditya Yadav <aditya78624@gmail.com>
2021-08-03 23:14:48 +0530

    added 2 pages

---
+++ Assets/js/my.js
@@ -0,0 +1,39 @@
+
+//function for age
+function setAge(e) {
+  var bday = new Date(Date.parse(e.target.value));
+  var today = new Date()
+
+  document.getElementsByName('age')[0].value = today.getFullYear() - bday.getFullYear();
+}
+
+
+// initializing a buffer object
+
+// let data = {
+//   username : '',
+//   stream : ''
+// }
+
+//funtion for retrieving data
+
+function saveFile() {
+
+  // Get the data from each element on the form.
+  let name = document.getElementById('nameId');
+  let stream = document.getElementById('streamId');
+
+
+  // data['username'] = name.value;
+  // data['stream'] = stream.value;
+  //export {data};
+  window.sessionStorage.setItem('nameId', name.value);
+  window.sessionStorage.setItem('streamId', stream.value);
+
+  // setting value for name and stream in datasheet page
+
+  
+  //window.location.replace("./dataSheet.html");
+}
+
+//export {data};
